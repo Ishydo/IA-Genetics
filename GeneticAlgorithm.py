d33cdc33332ef9186b7e9dcc70f9bfbf7c67e71f
dominiqu.ducommun <dominique.ducommun@he-arc.ch>
2016-12-12 10:39:09 +0100

    GeneticAlgorithm pseudo code

---
+++ GeneticAlgorithm.py
@@ -0,0 +1,14 @@
+# Voir document GA.pdf
+
+# Soit une population initiale
+
+# Tant que la règle n'est pas satisfaite
+# do
+#   Soit X(n) la population courante
+#   Évaluer le degré d'adaptation de chaque individu
+#   Sélectionner dans X(n) un ensemble de paires de solutions high quality
+#   Appliquer à chacune des paires de soluions sélectionnées un opérateur de croisement
+#   Remplacer une partie de X(n), formée des solutions basse qualité par des enfants de haute qualité
+#   Appliquer un opérateur de mutation aux solutions ainsi obtenues
+#   Les solutions éventuellement mutées constituent la population X(n+1)
+# end
